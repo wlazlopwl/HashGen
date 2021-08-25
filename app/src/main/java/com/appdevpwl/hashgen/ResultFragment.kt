cbd0d580571661759f9d49febf08280f3d844e59
Pawel Wlazlo <wlazlopwl@gmail.com>
2021-08-25 13:58:44 +0200

    add copy hash function

--- app/src/main/java/com/appdevpwl/hashgen/ResultFragment.kt
+++ app/src/main/java/com/appdevpwl/hashgen/ResultFragment.kt
@@ -1,9 +1,13 @@
 package com.appdevpwl.hashgen
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.appdevpwl.hashgen.databinding.FragmentResultBinding
@@ -24,9 +28,17 @@ class ResultFragment : Fragment() {
         _binding = FragmentResultBinding.inflate(inflater, container, false)
 
         binding.tvHash.text=args?.hashString
-
+        binding.btnCopy.setOnClickListener {
+            copyToClipboard(args.toString())
+        }
 
         return binding.root
+    }
+
+    private fun copyToClipboard(hash: String) {
+        val clipboard = requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val copyText: ClipData = ClipData.newPlainText("hash", hash)
+        clipboard.setPrimaryClip(copyText)
     }
 
 
